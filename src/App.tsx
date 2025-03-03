23e74165ea7b08e5b0ff8f7c77d39d789246ffb2
Eliot Hills <j.eliot.hills@gmail.com>
2025-03-03 10:33:05 -0400

    Implement navbar

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import classNames from 'classnames'
 import { replace } from 'util/replace'
 
+import logoUrl from 'assets/sct_logo.png'
+
 export interface Dataset {
   label: string
   count: number
@@ -14,25 +16,45 @@ function App() {
   const [datasets, setDatasets] = useState<Array<Dataset>>(INITIAL_DATASETS)
 
   return (
-    <div className="h-dvh flex flex-col p-4 items-center justify-center">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <>
+      <nav className="min-h-12 mr-auto ml-auto px-4 border-b-2 border-gray-200 bg-gray-100">
+        <div className="flex flex-row flex-nowrap justify-between items-center">
+          <a href="#" className="text-gray-500 hover:text-gray-700">
+            <div className="flex flex-row flex-nowrap items-center">
+              <img src={logoUrl} className="h-7" />
+              <span className="h-12 p-4 text-lg leading-5">
+                SCT - Quality Control
+              </span>
+            </div>
+          </a>
+          <a
+            href="https://spinalcordtoolbox.com/"
+            className="text-sm text-gray-400 hover:text-gray-700"
+          >
+            SCT Home
+          </a>
+        </div>
+      </nav>
+      <div className="h-dvh flex flex-col p-4 items-center justify-center">
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
 
-      {datasets.map(({ label, count }, i) => (
-        <button
-          key={i}
-          className={classNames(
-            'mt-4 p-2 py-0 border-black/50 border-2 rounded-sm max-w-fit',
-            'cursor-pointer font-bold transition-colors',
-            'hover:bg-slate-300',
-          )}
-          onClick={() =>
-            setDatasets(replace(datasets, i, { label, count: count + 1 }))
-          }
-        >
-          {label} count is {count}
-        </button>
-      ))}
-    </div>
+        {datasets.map(({ label, count }, i) => (
+          <button
+            key={i}
+            className={classNames(
+              'mt-4 p-2 py-0 border-black/50 border-2 rounded-sm max-w-fit',
+              'cursor-pointer font-bold transition-colors',
+              'hover:bg-slate-300',
+            )}
+            onClick={() =>
+              setDatasets(replace(datasets, i, { label, count: count + 1 }))
+            }
+          >
+            {label} count is {count}
+          </button>
+        ))}
+      </div>
+    </>
   )
 }
 
